b7464cc76197bf70138e43bf6a6c3e20f64665b3
yongjie_zou <zouyongjie@baijiahulian.com>
2016-03-19 17:13:18 +0800

    key存在，而value为nil时，想改变其value

--- Test/KeyValueTest.m
+++ Test/KeyValueTest.m
@@ -17,15 +17,25 @@
     
 //    [testInstance setValue:nil forKey:@""];    //this class is not key value coding-compliant for the key
     if ([testInstance valueForKey:@"name"]) {
-        NSLog(@"name:%@", [testInstance valueForKey:@"name"]);
+        NSLog(@"name first:%@", [testInstance valueForKey:@"name"]);
         [testInstance setValue:nil forKey:@"name"]; //有对应的属性， value可以为nil
     }
     
     
-    //  ********    事实上@"phone"这个key是存在的，只是值本身就为nil
+    //  ********    事实上@"phone"这个key是存在的，只是值本身就为nil   这样岂不是无法赋值？？？？？
     if ([testInstance valueForKey:@"phone"]) {
-        NSLog(@"phone:%@", [testInstance valueForKey:@"phone"]);
+        NSLog(@"phone first:%@", [testInstance valueForKey:@"phone"]);
         [testInstance setValue:nil forKey:@"phone"];
+    }
+    
+    if ([testInstance respondsToSelector:NSSelectorFromString(@"phone")]) {
+         NSLog(@"phone second:%@", [testInstance valueForKey:@"phone"]);
+        [testInstance setValue:nil forKey:@"phone"];       
+    }
+    
+    if ([testInstance respondsToSelector:NSSelectorFromString(@"name")]) {
+         NSLog(@"name second:%@", [testInstance valueForKey:@"name"]);
+        [testInstance setValue:nil forKey:@"name"]; //有对应的属性， value可以为nil       
     }
 }
 
